d1be10644b3a0e6c110e0d023714681a489c1368
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-10 15:00:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -264,10 +264,7 @@ def player_with_longest_name
   game_hash
   array = []
   array << game_hash[:home][:players][0].keys[0].length
-  array << game_hash[:home][:players][0].values[1][:shoe]
-  array << game_hash[:home][:players][0].values[2][:shoe]
-  array << game_hash[:home][:players][0].values[3][:shoe]
-  array << game_hash[:home][:players][0].values[4][:shoe]
+array << game_hash[:home][:players][0].keys[0].length
   
   array << game_hash[:away][:players][0].values[0][:shoe]
   array << game_hash[:away][:players][0].values[1][:shoe]

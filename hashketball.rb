df0a07c70bc3cf2ec34fb4df65062060515accb5
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-10 14:44:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -213,7 +213,7 @@ def big_shoe_rebounds
   array << game_hash[:away][:players][0].values[2][:shoe]
   array << game_hash[:away][:players][0].values[3][:shoe]
   array << game_hash[:away][:players][0].values[4][:shoe]
-  array.sort
-  array[0]
+  newArray = array.sort
+  newArray
 
 end

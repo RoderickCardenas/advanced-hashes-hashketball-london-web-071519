54acf336420fb60a7b3fd45cf4eafbc17095763c
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-10 13:46:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -171,7 +171,7 @@ def player_number(string)
     game_hash.each do |homeAwayKey, teamColorsPlayers|
         teamColorsPlayers.each do |teamNameColorsPlayers, valuesNameColorsPlayers|
           if valuesNameColorsPlayers[0].include?(string)
-            return valuesNameColorsPlayers[0][string][:points]
+            return valuesNameColorsPlayers[0][string][:number]
           end
         end
     end

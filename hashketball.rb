cb0a803bbbde31cb90bb2e48f35fb077e22d8df7
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-10 14:51:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -245,14 +245,14 @@ def winning_team
   array << game_hash[:home][:players][0].values[2][:points]
   array << game_hash[:home][:players][0].values[3][:points]
   array << game_hash[:home][:players][0].values[4][:points]
-  
+  home = homeTeam.sort
   awayTeam = []
   array << game_hash[:away][:players][0].values[0][:points]
   array << game_hash[:away][:players][0].values[1][:points]
   array << game_hash[:away][:players][0].values[2][:points]
   array << game_hash[:away][:players][0].values[3][:points]
   array << game_hash[:away][:players][0].values[4][:points]
-  home = homeTeam.sort
+  
   newArray
   binding.pry
   return game_hash[:away][:players][0].keys[3]

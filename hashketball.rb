ada76ba0542a1b1d801d38044b1088bbb971172c
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-10 13:08:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -129,9 +129,9 @@ def num_points_scored(string)
   game_hash
     game_hash.each do |homeAwayKey, teamColorsPlayers|
         teamColorsPlayers.each do |teamNameColorsPlayers, valuesNameColorsPlayers|
-          valuesNameColorsPlayers.each do |players|
-        binding.pry
-          end
+
+          binding.pry
+          
         end
     end
 end
